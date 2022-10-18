7ebe10caf834f6b8b492b60343f463aa1c22c462
Valeria Manriquez <vmanriquez@sourcing.cl>
2022-10-17 23:12:06 -0300

    main js configuration

--- app/src/main.js
+++ app/src/main.js
@@ -1,4 +1,4 @@
-import Vue from 'vue';
+import { createApp } from 'vue';
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 import VueForm from 'vue-form';
 import VueAxios from 'vue-axios';
@@ -10,18 +10,17 @@ import App from './App.vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
+const app = createApp(App);
+app.use(router);
 // Make BootstrapVue available throughout your project
-Vue.use(BootstrapVue);
+app.use(BootstrapVue);
 // Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin);
+app.use(IconsPlugin);
 // Use Vue Form
-Vue.use(VueForm);
+app.use(VueForm);
 // Axios
-Vue.use(VueAxios, axios);
+app.use(VueAxios, axios);
 
-Vue.config.productionTip = false;
+app.config.productionTip = false;
 
-new Vue({
-  router,
-  render: (h) => h(App),
-}).$mount('#app');
+createApp(App).mount('#app');
